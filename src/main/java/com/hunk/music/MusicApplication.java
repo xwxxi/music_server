3cf18d05922f3008c1c794ecc7ad06130b4aae75
xwxxi <1923656649@qq.com>
2023-03-05 02:20:47 +0800

    初始化项目

---
+++ src/main/java/com/hunk/music/MusicApplication.java
@@ -0,0 +1,14 @@
+package com.hunk.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+    // 启动
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class, args);
+    }
+
+}
